15c0f6d55bec496e0301881a40aefd5b5a93f3a1
Jakub <kubtet1@wp.pl>
2023-03-23 13:00:10 +0100

    End of section 14

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -11,7 +11,8 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
+  constructor(public accountService: AccountService, private router: Router, 
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
