b29a39ab72aa5ee120718e5f8d82ee6c6917da5a
Jakub <kubtet1@wp.pl>
2023-04-05 14:01:52 +0200

    End of challenge

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -45,7 +45,9 @@ namespace API.Controllers
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _uow.UserRepository.GetMemberAsync(username);
+            var currentUsername = User.GetUsername();
+            return await _uow.UserRepository.GetMemberAsync(username,
+                isCurrentUser: currentUsername == username);
         }
 
         [HttpPut]
@@ -78,8 +80,6 @@ namespace API.Controllers
                 Url = result.SecureUrl.AbsoluteUri,
                 PublicId = result.PublicId
             };
-
-            if (user.Photos.Count == 0) photo.IsMain = true;
 
             user.Photos.Add(photo);
 
@@ -119,7 +119,7 @@ namespace API.Controllers
         {
             var user = await _uow.UserRepository.GetUserByUsernameAsync(User.GetUsername());
 
-            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+            var photo = await _uow.PhotoRepository.GetPhotoById(photoId);
 
             if(photo == null) return NotFound();
 
